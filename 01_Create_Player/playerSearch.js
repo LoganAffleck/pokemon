f99848702e2a13dfbbce1880950f461320ee2798
J. Logan Affleck <affleck.logan@gmail.com>
2025-04-28 18:25:51 -0400

    Create playerSearch.js

---
+++ 01_Create_Player/playerSearch.js
@@ -0,0 +1,36 @@
+import player from "./player.js"
+
+// Challenge 1: Create a function to search for any item in the bag
+// ➤ Your task: Write a function called `searchBag` that takes two parameters:
+//    1. `itemName`: The name of the item you want to search for (e.g., "Potion").
+//    2. `pocket`: The pocket in your bag where you want to search (e.g., "potions").
+//
+
+player.searchBag = function (itemName, pocket) {}
+
+// Challenge 2: Create a function to search for the player's strongest Pokémon
+// ➤ Your task: Write a function called `findStrongestPokemon` that takes no parameters.
+
+player.findStrongestPokemon = function () {}
+
+
+// Challenge 3: Create a function to search for the player's fastest Pokémon
+// ➤ Your task: Write a function called `findFastestPokemon` that takes no parameters.
+
+player.findFastestPokemon = function () {}
+
+// Challenge 4: Experience points and leveling up
+// ➤ Your task: Write a loop that adds the attribute "experience" to each Pokémon in the bag.
+//    1. Each Pokémon should start with 0 experience points.
+
+
+
+// Challenge 5: Create a function to add experience points to your Pokémon team.
+// ➤ Your task: Write a function called `experienceShare` that takes one parameter:
+//    1. `points`: The number of experience points to add to each Pokémon in the bag.
+//    2. The function should add the points to each Pokémon's experience attribute and log the updated Pokémon to the console.
+//    3. After each pokemon has recieved experience points, the next pokemon should recieve 30% less experience points than the previous one.
+
+player.experienceShare = function (points){
+
+}
